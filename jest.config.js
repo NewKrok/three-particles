4641494040a15c457b490aeaee7216346008af36
Krisztian Somoracz <krisztian.somoracz@gmail.com>
2025-01-11 01:41:42 +0100

    chore: first test with github action + related changes

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.m?[tj]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^(.+)\\.js$': '$1',
+  },
+  transformIgnorePatterns: ['node_modules/(?!three-noise|three|@newkrok/three-utils)'],
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+};
